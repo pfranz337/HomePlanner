5abef69612bbf1c08a2dea6f42411f08f994c554
Petr Franz <petr.franz@ibrconsulting.cz>
2025-05-18 14:39:58 +0200

    refactor files a references + namespaces

--- HPAdmin.UI/Extensions.cs
+++ HPAdmin.UI/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using HPAdmin.Data.Dto;
 using HPAdmin.UI.Models.Base;
 
+namespace HPAdmin.UI;
+
 public static class Extensions
 {
     public static TDto[] GetDtoList<TDto, TModel>(this IEnumerable<TModel> models)
